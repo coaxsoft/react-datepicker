39e23762dc893846272ab0e20173a350748a78b9
Taras Danyliuk <taras.danylyuk@coaxsoft.com>
2019-05-14 14:49:58 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#React-Datepicker-on-Steroids
+
+###Installation
+```
+none
+```
+
+###License
+[ISC](https://www.isc.org/downloads/software-support-policy/isc-license/)
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+
+class DatePicker extends React.Component {
+  render() {
+    return (
+      <div className="rdos-wrapper">
+        <input/>
+      </div>
+    )
+  }
+}
+
+export default DatePicker;
