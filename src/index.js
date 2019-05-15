6ac3b8ee29053b2a9d853fcc2d425603a56407ba
Taras Danyliuk <taras.danylyuk@coaxsoft.com>
2019-05-15 11:12:11 +0300

    add popup on input click, hide it on click outside

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
 import "./styles.css";
 
+import Datepicker from "./Datepicker";
 
-class DatePicker extends React.Component {
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._triggerDatepicker = this._triggerDatepicker.bind(this);
+    this.removeDatepicker = this.removeDatepicker.bind(this);
+  }
+
+  _triggerDatepicker() {
+    this.popup = document.createElement("div");
+    this.popup.setAttribute("id", "rdospopupTarget");
+
+    document.body.appendChild(this.popup);
+
+    ReactDOM.render(<Datepicker onClickOutside={this.removeDatepicker}/>, this.popup);
+  }
+
+  removeDatepicker() {
+    ReactDOM.unmountComponentAtNode(this.popup);
+    document.body.removeChild(this.popup);
+  }
+
+
   render() {
     return (
-      <div className="rdos-wrapper">
+      <div className="rdos-wrapper" onClick={this._triggerDatepicker}>
         <input/>
       </div>
     )
   }
 }
 
-export default DatePicker;
+export default Main;
